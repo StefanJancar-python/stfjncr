4e9baebd2014c1f6653ef060eab68107c5c92eaa
vianoce <stefiec@azet.sk>
2021-01-10 16:01:29 +0100

    osobny-web, first commit

--- flaskr/config.py
+++ flaskr/config.py
@@ -3,9 +3,5 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
-
-
-class Config(object):
-
-    SECRET_KEY = os.environ.get('SECRET_KEY') or b'\nN\xff{\x9d\x11S\xd5\x82=)\x14\x93\xe2Dv'
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+SECRET_KEY = os.environ.get('SECRET_KEY') or b'\nN\xff{\x9d\x11S\xd5\x82=)\x14\x93\xe2Dv'
+LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
